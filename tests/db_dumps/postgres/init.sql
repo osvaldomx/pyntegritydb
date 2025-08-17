1e90ab2673cda1ec659de02c936da2b0f4296ed9
Osvaldo Trujillo <osvaldo.trujillo.ingenieria@gmail.com>
2025-08-17 13:23:28 -0600

    [FIX] tests db workflow.

--- tests/db_dumps/postgres/init.sql
+++ tests/db_dumps/postgres/init.sql
@@ -16,14 +16,14 @@ CREATE TABLE "orders" (
 );
 
 -- Desactivar temporalmente las restricciones de FK para la sesión actual
--- SET session_replication_role = 'replica';
+SET session_replication_role = 'replica';
 
--- INSERT INTO "users" ("id", "name") VALUES (1, 'Alice'), (2, 'Bob');
--- INSERT INTO "orders" ("order_id", "user_id", "product", "customer_name") VALUES 
--- (101, 1, 'Laptop', 'Alice'),
--- (102, 2, 'Mouse', 'Bob'),
--- (103, 1, 'Keyboard', 'Alicia'),
--- (104, 99, 'Monitor', 'Charlie');
+INSERT INTO "users" ("id", "name") VALUES (1, 'Alice'), (2, 'Bob');
+INSERT INTO "orders" ("order_id", "user_id", "product", "customer_name") VALUES 
+(101, 1, 'Laptop', 'Alice'),
+(102, 2, 'Mouse', 'Bob'),
+(103, 1, 'Keyboard', 'Alicia'),
+(104, 99, 'Monitor', 'Charlie');
 
 -- Desactivar temporalmente las restricciones de FK para la sesión actual
--- SET session_replication_role = 'replica';
+SET session_replication_role = 'replica';
