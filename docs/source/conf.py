ab28df1a1bab4ec366f34129e4fd688519f2e810
Osvaldo Trujillo <osvaldo.trujillo.ingenieria@gmail.com>
2025-08-16 23:50:48 -0600

    [ADD] Sphinx for documentation.

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+sys.path.insert(0, os.path.abspath('../../pyntegrity'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'pyntegritydb'
+copyright = '2025, Osvaldo Trujillo'
+author = 'Osvaldo Trujillo'
+release = '0.2.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',      # Importa la documentación desde los docstrings
+    'sphinx.ext.napoleon',     # Soporta docstrings de Google y NumPy
+    'sphinx.ext.viewcode',     # Añade enlaces al código fuente
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'es'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/source/modulos.rst
@@ -0,0 +1,52 @@
+Referencia de la API
+====================
+
+Esta sección contiene la documentación detallada de cada módulo en ``pyntegritydb``, generada automáticamente a partir del código fuente.
+
+Módulo `connect`
+-----------------
+
+.. automodule:: pyntegritydb.connect
+   :members:
+
+Módulo `metrics`
+-----------------
+
+.. automodule:: pyntegritydb.metrics
+   :members:
+
+Módulo `schema`
+-----------------
+
+.. automodule:: pyntegritydb.schema
+   :members:
+
+Módulo `alerts`
+-----------------
+
+.. automodule:: pyntegritydb.alerts
+   :members:
+
+Módulo `report`
+-----------------
+
+.. automodule:: pyntegritydb.report
+   :members:
+
+Módulo `visualize`
+------------------
+
+.. automodule:: pyntegritydb.visualize
+   :members:
+
+Módulo `cli`
+-----------------
+
+.. automodule:: pyntegritydb.cli
+   :members:
+
+Módulo `config`
+-----------------
+
+.. automodule:: pyntegritydb.config
+   :members:
